1513c65037cc7bfca60d6269b2f81b9a6e87ce1b
ChanHyuk <whk1140@gmail.com>
2021-08-22 11:42:23 +0900

    versionInfo add

--- KakaoTalk/KakaoTalk/lab.swift
+++
@@ -1,8 +0,0 @@
-//
-//  lab.swift
-//  KakaoTalk
-//
-//  Created by ChanHyuk Kim on 2021/08/21.
-//
-
-import Foundation
---
+++ KakaoTalk/KakaoTalk/seeMore/versionInfo.swift
@@ -0,0 +1,8 @@
+//
+//  versionInfo.swift
+//  KakaoTalk
+//
+//  Created by ChanHyuk Kim on 2021/08/22.
+//
+
+import Foundation
