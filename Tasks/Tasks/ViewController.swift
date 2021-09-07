0d0943e5afb7c4b33ba6de29baaa09f063798e70
ChanHyuk <whk1140@gmail.com>
2021-09-07 18:01:56 +0900

    testing to do list

--- Tasks/Tasks/ViewController.swift
+++ Tasks/Tasks/ViewController.swift
@@ -22,15 +22,18 @@ class ViewController: UIViewController {
     }
     
     func updateTasks() {
+        
+        tasks.removeAll()
         guard let count = UserDefaults().value(forKey: "count") as? Int else {
             return
         }
         
-        for x in 0...count {
-            
-            
+        for x in 0..<count {
+            if let task = UserDefaults().value(forKey: "task_\(x + 1)") as? String {
+                tasks.append(task)
+            }
         }
-        
+        tableView.reloadData()
     }
     
     @IBAction func didTapAdd() {
