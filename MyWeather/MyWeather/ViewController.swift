920b37db90ac44a17ba5dcff6a65768ba260ed79
ChanHyuk <whk1140@gmail.com>
2021-09-24 17:34:09 +0900

    API add configure func error

--- MyWeather/MyWeather/ViewController.swift
+++ MyWeather/MyWeather/ViewController.swift
@@ -1,22 +1,27 @@
 import UIKit
-import CoreLocation
+import CoreLocation // 주변 아이비컨과 관련된 지리적 위치 서비스를 제공
 
+
+// 테이블 뷰를 사용하기 위해서는 UITableViewDelegate, UITableViewDataSource 프로토콜을 사용해야 한다.
+// CLLocationManagerDelegate
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+    
+    // 테이블 뷰의 행의개수 위에서부터 한줄씩의 갯수
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return models.count
     }
     
+    // cell의 갯수
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier, for: indexPath) as! WeatherTableViewCell
+        cell.configure(to: models[indexPath.row])
+        return cell
     }
     
+    @IBOutlet var table: UITableView! // 스토리보드에 만들어 놓은 테이블 뷰를 연결함
     
-    @IBOutlet var table: UITableView!
-    
-    var models = [Weather]()
-    
+    var models = [DailyWeatherEntry]()
     let locationManger = CLLocationManager()
-    
     var currentLocation: CLLocation?
 
     override func viewDidLoad() {
@@ -26,7 +31,6 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         table.delegate = self
         table.dataSource = self
-        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -55,13 +59,137 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         let long = currentLocation.coordinate.longitude
         let lat = currentLocation.coordinate.latitude
-        
-        print("\(long) | \(lat)")
+        let url = "https://api.darksky.net/forecast/ddcc4ebb2a7c9930b90d9e59bda0ba7a/\(lat),\(long)?exclude=[flags,minutely]"
+        URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+            // Validation
+            guard let data = data, error == nil else {
+                print("something went wrong")
+                return
+            }
+            
+            // Convert data to models/some object
+            var json: WeatherResponse?
+            do {
+                json = try JSONDecoder().decode(WeatherResponse.self, from: data)
+            }
+            catch {
+                print("error: \(error)")
+            }
+            guard let result = json else {
+                return
+            }
+            
+            let entries = result.daily.data
+            
+            self.models.append(contentsOf: entries)
+            // update user interface
+            DispatchQueue.main.async {
+                self.table.reloadData()
+            }
+            
+        }).resume()
     }
     
-    struct Weather {
-        
+    struct WeatherResponse: Codable {
+        let latitude: Float
+        let longitude: Float
+        let timezone: String
+        let currently: CurrentWeather
+        let hourly: HourlyWeather
+        let daily: DailyWeather
+        let offset: Float
     }
+    
+    struct CurrentWeather: Codable{
+        let time: Int
+        let summary: String
+        let icon: String
+        let nearestStormDistance: Int
+        let nearestStormBearing: Int
+        let precipIntensity: Int
+        let precipProbability: Int
+        let temperature: Double
+        let apparentTemperature: Double
+        let dewPoint: Double
+        let humidity: Double
+        let pressure: Double
+        let windSpeed: Double
+        let windGust: Double
+        let windBearing: Int
+        let cloudCover: Double
+        let uvIndex: Int
+        let visibility: Double
+        let ozone: Double
+    }
+    
+    struct DailyWeather: Codable {
+        let summary: String
+        let icon: String
+        let data: [DailyWeatherEntry]
+    }
+    struct DailyWeatherEntry: Codable {
+        let time: Int
+        let summary: String
+        let icon: String
+        let sunriseTime: Int
+        let sunsetTime: Int
+        let moonPhase: Double
+        let precipIntensity: Float
+        let precipIntensityMax: Float
+        let precipIntensityMaxTime: Int
+        let precipProbability: Double
+        let precipType: String?
+        let temperatureHigh: Double
+        let temperatureHighTime: Int
+        let temperatureLow: Double
+        let temperatureLowTime: Int
+        let dewPoint: Double
+        let humidity: Double
+        let pressure: Double
+        let windSpeed: Double
+        let windGust: Double
+        let windGustTime: Int
+        let windBearing: Int
+        let cloudCover: Double
+        let uvIndex: Int
+        let uvIndexTime: Int
+        let visibility: Double
+        let ozone: Double
+        let temperatureMin: Double
+        let temperatureMinTime: Int
+        let apparentTemperatureMin: Double
+        let apparentTemperatureMinTime: Int
+        let apparentTemperatureMax: Double
+        let apparentTemperatureMaxTime: Int
+    }
+    
+    struct HourlyWeather: Codable {
+        let summary: String
+        let icon: String
+        let data: [HourlyWeatherEntry]
+    }
+    
+    struct HourlyWeatherEntry: Codable {
+        let time: Int
+        let summary: String
+        let icon: String
+        let precipIntensity: Float
+        let precipProbability: Double
+        let precipType: String?
+        let temperature: Double
+        let apparentTemperature: Double
+        let dewPoint: Double
+        let humidity: Double
+        let pressure: Double
+        let windSpeed: Double
+        let windGust: Double
+        let windBearing: Int
+        let cloudCover: Double
+        let uvIndex: Int
+        let visibility: Double
+        let ozone: Double
+    }
+    
 
 }
 
---
+++ MyWeather/README.md
@@ -0,0 +1,8 @@
+# MyWeather
+
+## 위치정보는 사용자의 개인정보이므로 함부로 앱이 접근할 수 없기 때문에 따로 허가를 받아야 한다.
+
+Privacy - Location When In Use Usage Description 앱을 실행하고 있을 때만 위치 정보 접근하고 이용함
+Privacy - Location Usage Description  위치정보를 이용함
+Privacy - Location Always Usage Description 항상 위치정보에 접근
+Privacy - Location Always and When In Use Usage Description 항상 위치정보에 접근하고 위치정보를 이용함
