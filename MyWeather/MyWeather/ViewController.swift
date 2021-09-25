8a8f5a9f4fe75e7151bbcea501fb2764163e568c
ChanHyuk <whk1140@gmail.com>
2021-09-25 14:32:49 +0900

    error correct

--- MyWeather/MyWeather/ViewController.swift
+++ MyWeather/MyWeather/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // cell의 갯수
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier, for: indexPath) as! WeatherTableViewCell
-        cell.configure(to: models[indexPath.row])
+        cell.configure(with: models[indexPath.row])
         return cell
     }
     
@@ -90,106 +90,104 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }).resume()
     }
     
-    struct WeatherResponse: Codable {
-        let latitude: Float
-        let longitude: Float
-        let timezone: String
-        let currently: CurrentWeather
-        let hourly: HourlyWeather
-        let daily: DailyWeather
-        let offset: Float
-    }
-    
-    struct CurrentWeather: Codable{
-        let time: Int
-        let summary: String
-        let icon: String
-        let nearestStormDistance: Int
-        let nearestStormBearing: Int
-        let precipIntensity: Int
-        let precipProbability: Int
-        let temperature: Double
-        let apparentTemperature: Double
-        let dewPoint: Double
-        let humidity: Double
-        let pressure: Double
-        let windSpeed: Double
-        let windGust: Double
-        let windBearing: Int
-        let cloudCover: Double
-        let uvIndex: Int
-        let visibility: Double
-        let ozone: Double
-    }
-    
-    struct DailyWeather: Codable {
-        let summary: String
-        let icon: String
-        let data: [DailyWeatherEntry]
-    }
-    struct DailyWeatherEntry: Codable {
-        let time: Int
-        let summary: String
-        let icon: String
-        let sunriseTime: Int
-        let sunsetTime: Int
-        let moonPhase: Double
-        let precipIntensity: Float
-        let precipIntensityMax: Float
-        let precipIntensityMaxTime: Int
-        let precipProbability: Double
-        let precipType: String?
-        let temperatureHigh: Double
-        let temperatureHighTime: Int
-        let temperatureLow: Double
-        let temperatureLowTime: Int
-        let dewPoint: Double
-        let humidity: Double
-        let pressure: Double
-        let windSpeed: Double
-        let windGust: Double
-        let windGustTime: Int
-        let windBearing: Int
-        let cloudCover: Double
-        let uvIndex: Int
-        let uvIndexTime: Int
-        let visibility: Double
-        let ozone: Double
-        let temperatureMin: Double
-        let temperatureMinTime: Int
-        let apparentTemperatureMin: Double
-        let apparentTemperatureMinTime: Int
-        let apparentTemperatureMax: Double
-        let apparentTemperatureMaxTime: Int
-    }
-    
-    struct HourlyWeather: Codable {
-        let summary: String
-        let icon: String
-        let data: [HourlyWeatherEntry]
-    }
-    
-    struct HourlyWeatherEntry: Codable {
-        let time: Int
-        let summary: String
-        let icon: String
-        let precipIntensity: Float
-        let precipProbability: Double
-        let precipType: String?
-        let temperature: Double
-        let apparentTemperature: Double
-        let dewPoint: Double
-        let humidity: Double
-        let pressure: Double
-        let windSpeed: Double
-        let windGust: Double
-        let windBearing: Int
-        let cloudCover: Double
-        let uvIndex: Int
-        let visibility: Double
-        let ozone: Double
-    }
-    
-
 }
 
+struct WeatherResponse: Codable {
+    let latitude: Float
+    let longitude: Float
+    let timezone: String
+    let currently: CurrentWeather
+    let hourly: HourlyWeather
+    let daily: DailyWeather
+    let offset: Float
+}
+
+struct CurrentWeather: Codable{
+    let time: Int
+    let summary: String
+    let icon: String
+    let nearestStormDistance: Int
+    let nearestStormBearing: Int
+    let precipIntensity: Int
+    let precipProbability: Int
+    let temperature: Double
+    let apparentTemperature: Double
+    let dewPoint: Double
+    let humidity: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windGust: Double
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let visibility: Double
+    let ozone: Double
+}
+
+struct DailyWeather: Codable {
+    let summary: String
+    let icon: String
+    let data: [DailyWeatherEntry]
+}
+struct DailyWeatherEntry: Codable {
+    let time: Int
+    let summary: String
+    let icon: String
+    let sunriseTime: Int
+    let sunsetTime: Int
+    let moonPhase: Double
+    let precipIntensity: Float
+    let precipIntensityMax: Float
+    let precipIntensityMaxTime: Int
+    let precipProbability: Double
+    let precipType: String?
+    let temperatureHigh: Double
+    let temperatureHighTime: Int
+    let temperatureLow: Double
+    let temperatureLowTime: Int
+    let dewPoint: Double
+    let humidity: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windGust: Double
+    let windGustTime: Int
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let uvIndexTime: Int
+    let visibility: Double
+    let ozone: Double
+    let temperatureMin: Double
+    let temperatureMinTime: Int
+    let apparentTemperatureMin: Double
+    let apparentTemperatureMinTime: Int
+    let apparentTemperatureMax: Double
+    let apparentTemperatureMaxTime: Int
+}
+
+struct HourlyWeather: Codable {
+    let summary: String
+    let icon: String
+    let data: [HourlyWeatherEntry]
+}
+
+struct HourlyWeatherEntry: Codable {
+    let time: Int
+    let summary: String
+    let icon: String
+    let precipIntensity: Float
+    let precipProbability: Double
+    let precipType: String?
+    let temperature: Double
+    let apparentTemperature: Double
+    let dewPoint: Double
+    let humidity: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windGust: Double
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let visibility: Double
+    let ozone: Double
+}
