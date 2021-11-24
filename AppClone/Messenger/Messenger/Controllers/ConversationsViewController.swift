15fa3edc55080288c8ade7cf496928dd724bad11
ChanHyuk <whk1140@gmail.com>
2021-11-24 17:03:34 +0900

    messenger add(Log In & Register Screens)

--- AppClone/Messenger/Messenger/Controllers/ConversationsViewController.swift
+++ AppClone/Messenger/Messenger/Controllers/ConversationsViewController.swift
@@ -1,17 +1,23 @@
-//
-//  ViewController.swift
-//  Messenger
-//
-//  Created by ChanHyuk Kim on 2021/11/22.
-//
-
 import UIKit
 
 class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
     }
 
 
