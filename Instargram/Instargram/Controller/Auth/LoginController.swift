fde600e903e462c66ae3cb5d6368bdd7a4ed4d2e
ChanHyuk <whk1140@gmail.com>
2022-01-26 15:08:59 +0900

    Instargram update(Login Page UI complete)

--- Instargram/Instargram/Controller/Auth/LoginController.swift
+++ Instargram/Instargram/Controller/Auth/LoginController.swift
@@ -23,6 +23,48 @@ class LoginController: UIViewController{
         return textField
     }()
     
+    private let passwordField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.textColor = .white
+        textField.keyboardAppearance = .dark
+        textField.keyboardType = .emailAddress
+        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        textField.setHeight(50)
+        textField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.setHeight(50)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        return button
+    }()
+    private let forgotButton: UIButton = {
+        let button = UIButton(type: .system)
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "Forgot your password?  ", attributes: atts)
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: "Get help signing in.", attributes: boldAtts))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
+    private let registerButton: UIButton = {
+        let button = UIButton(type: .system)
+        let atts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: atts)
+        let boldAtts: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7), .font: UIFont.boldSystemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: boldAtts))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,5 +85,15 @@ class LoginController: UIViewController{
         iconImage.centerX(inView: view)
         iconImage.setDimensions(height: 80, width: 120)
         iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailField, passwordField, loginButton,forgotButton])
+        // 계속해서 위치를 잡을 필요가 없이 stack view 하나만 위치를 잡아주면 나머지는 자동으로 위치가 잡힘 스택뷰의 장점
+        stack.axis = .vertical
+        stack.spacing = 20
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        view.addSubview(registerButton)
+        registerButton.centerX(inView: view)
+        registerButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
     }
 }
