9569b6403c407c71dcb52f6b1b9ac57c48208f1b
ChanHyuk <whk1140@gmail.com>
2022-01-28 14:30:44 +0900

    Instargram add(Setting Up Firebase)

--- Instargram/Instargram/AppDelegate.swift
+++ Instargram/Instargram/AppDelegate.swift
@@ -1,11 +1,5 @@
-//
-//  AppDelegate.swift
-//  Instargram
-//
-//  Created by ChanHyuk Kim on 2022/01/21.
-//
-
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
