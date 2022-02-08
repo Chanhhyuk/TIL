903ec9910ce0cba1600816a9dc0d0fc017f9db61
ChanHyuk <whk1140@gmail.com>
2022-02-08 13:22:46 +0900

    Instargram update(Setting Up Profile Collection View)

--- Instargram/Instargram/View/Profile/ProfileCell.swift
+++ Instargram/Instargram/View/Profile/ProfileCell.swift
@@ -3,6 +3,7 @@ import UIKit
 class ProfileCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
+        backgroundColor = .lightGray
     }
     
     required init?(coder: NSCoder) {
