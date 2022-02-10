62665f7c85ebda19bacc6b82e4c4d0164b06b1aa
ChanHyuk <whk1140@gmail.com>
2022-02-10 17:35:25 +0900

    Instargram update(Finishing Up Profile UI)

--- Instargram/Instargram/View/Profile/ProfileHeader.swift
+++ Instargram/Instargram/View/Profile/ProfileHeader.swift
@@ -28,8 +28,27 @@ class ProfileHeader: UICollectionReusableView {
         return button
     }()
     @objc private func tapEditProfileButton(){
-        
+        print("dsds")
     }
+    
+    private let postLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    private let followersLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    private let followingLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
     
     
     // MARK: LifeCycle
@@ -43,9 +62,20 @@ class ProfileHeader: UICollectionReusableView {
         
         addSubview(nameLabel)
         nameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, paddingTop: 12, paddingLeft: 12)
+        addSubview(editProfileButton)
+        editProfileButton.anchor(top: nameLabel.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 16, paddingLeft: 24, paddingRight: 24)
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private func attributedText(value: Int, label: String) -> NSAttributedString {
+        let attributedText = NSMutableAttributedString(string: "\(value)\n", attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: label, attributes: [.font: UIFont.systemFont(ofSize: 14), .foregroundColor: UIColor.lightGray ]))
+        return attributedText
+        
+    }
+    
 }
